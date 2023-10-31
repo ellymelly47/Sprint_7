34e2db5a539d509f3eef7b3d797cf7f0adc70a12
Evgeniya Berdnikova <ellymelly47@yandex.ru>
2023-10-31 10:29:35 +0300

    deleted_if

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -10,47 +10,41 @@ class TestCourierLogin:
     def setup(self):
         self.login = generate_random_string(10)
         self.password = generate_random_string(10)
-        self.is_courier_registered = False
 
-        if self.is_courier_registered:
-            payload = {
-                'login': self.login,
-                'password': self.password
-            }
-            requests.post(f'{self.BASE_URL}', data=payload)
+        payload = {
+            'login': self.login,
+            'password': self.password
+        }
+        requests.post(f'{self.BASE_URL}', data=payload)
 
     def teardown(self):
-        if self.is_courier_registered:
-            payload = {
-                "login": self.login,
-                "password": self.password
-            }
-            response = requests.post(f"{self.BASE_URL}/login", data=payload)
-            courier_id = response.json()["id"]
+        payload = {
+            "login": self.login,
+            "password": self.password
+        }
+        response = requests.post(f"{self.BASE_URL}/login", data=payload)
+        courier_id = response.json()["id"]
 
-            if courier_id is not None:
-                requests.delete(f"{self.BASE_URL}/{courier_id}")
+        requests.delete(f"{self.BASE_URL}/{courier_id}")
 
     @allure.title('Проверка успешной авторизации курьера')
     def test_courier_login_success(self):
 
-        if self.is_courier_registered:
-            payload = {
-                'login': self.login,
-                'password': self.password
-            }
-            response = requests.post(f'{self.BASE_URL}/login', data=payload)
+        payload = {
+            'login': self.login,
+            'password': self.password
+        }
+        response = requests.post(f'{self.BASE_URL}/login', data=payload)
 
-            assert response.status_code == 200
-            assert 'id' in response.json() and isinstance(response.json()['id'], int)
-            self.is_courier_registered = True
+        assert response.status_code == 200
+        assert 'id' in response.json() and isinstance(response.json()['id'], int)
 
     @allure.title('Проверка ошибки авторизации курьера: авторизация несуществующего курьера')
     def test_courier_login_nonexistent_courier_failed_login(self):
 
         payload = {
-            'login': self.login,
-            'password': self.password
+            'login': generate_random_string(10),
+            'password': generate_random_string(10)
         }
         response = requests.post(f'{self.BASE_URL}/login', data=payload)
 
@@ -60,30 +54,26 @@ class TestCourierLogin:
     @allure.title('Проверка ошибки авторизации курьера: запрос с некорректным значением "login"')
     def test_courier_login_incorrect_login_value_failed_login(self):
 
-        if self.is_courier_registered:
-            payload_login = {
-                'login': generate_random_string(10),
-                'password': self.password
-            }
-            response = requests.post(f'{self.BASE_URL}/login', data=payload_login)
+        payload_login = {
+            'login': generate_random_string(10),
+            'password': self.password
+        }
+        response = requests.post(f'{self.BASE_URL}/login', data=payload_login)
 
-            assert response.status_code == 404
-            assert response.json()['message'] == 'Учетная запись не найдена'
-            self.is_courier_registered = True
+        assert response.status_code == 404
+        assert response.json()['message'] == 'Учетная запись не найдена'
 
     @allure.title('Проверка ошибки авторизации курьера: запрос с некорректным значением "password"')
     def test_courier_login_incorrect_password_value_failed_login(self):
 
-        if self.is_courier_registered:
-            payload_login = {
-                'login': self.login,
-                'password': generate_random_string(10)
-            }
-            response = requests.post(f'{self.BASE_URL}/login', data=payload_login)
+        payload_login = {
+            'login': self.login,
+            'password': generate_random_string(10)
+        }
+        response = requests.post(f'{self.BASE_URL}/login', data=payload_login)
 
-            assert response.status_code == 404
-            assert response.json()['message'] == 'Учетная запись не найдена'
-            self.is_courier_registered = True
+        assert response.status_code == 404
+        assert response.json()['message'] == 'Учетная запись не найдена'
 
     @allure.title('Проверка ошибки авторизации курьера: запросы без обязательных полей "login"/"password"')
     @pytest.mark.parametrize('payload', [
--- tests/test_get_one_order.py
+++ tests/test_get_one_order.py
@@ -7,27 +7,21 @@ class TestGetOneOrder:
     BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
 
     def setup(self):
-        self.track_id = None
-        self.is_order_created = False
-
-        if self.is_order_created:
-            order_response = requests.post(f'{self.BASE_URL}', json=OrderData.order_data)
-            self.track_id = order_response.json()['track']
+        order_response = requests.post(f'{self.BASE_URL}', json=OrderData.order_data)
+        self.track_id = order_response.json()['track']
 
     def teardown(self):
-        if self.track_id is not None:
-            payload = {'track': self.track_id}
-            requests.put(f'{self.BASE_URL}/cancel', data=payload)
+        payload = {'track': self.track_id}
+        requests.put(f'{self.BASE_URL}/cancel', data=payload)
 
     @allure.title('Проверка успешного получения информации по заказу')
     def test_get_one_order_successful_resp(self):
 
-        if self.is_order_created:
-            payload = {'t': self.track_id}
-            response = requests.get(f'{self.BASE_URL}/track', params=payload)
+        payload = {'t': self.track_id}
+        response = requests.get(f'{self.BASE_URL}/track', params=payload)
 
-            assert response.status_code == 200
-            assert response.json()['order']['track'] == self.track_id
+        assert response.status_code == 200
+        assert response.json()['order']['track'] == self.track_id
 
     @allure.title('Проверка ошибки получения информации по заказу: запрос с несуществующим номером заказа')
     def test_get_one_order_incorrect_id_error_resp(self):
